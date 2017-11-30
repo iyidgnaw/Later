d67f84b724af198802d28aa0dbd40b34221cef8b
Ubuntu <ubuntu@ip-172-31-2-36.us-west-1.compute.internal>
2017-11-30 07:38:32 +0000

    Add new task to list.
    Clean up code.

---
+++ util.py
@@ -0,0 +1,22 @@
+"""This is the util lib for Later App"""
+import pickle
+
+class LaterTask(object):
+    """Later App Task class"""
+    def __init__(self, task_id, time, content):
+        self.task_id = task_id
+        self.time = time
+        self.content = content
+
+    def __repr__(self):
+        return '{}: {}, scheduled {}'.format(
+            self.task_id, self.content, self.time)
+
+def load_from_file(filename):
+    """Helper function combines the open and pickle.load"""
+    return pickle.load(open(filename, 'rb'))
+
+
+def save_to_file(target_obj, filename):
+    """Helper function combines the open and pickle.dump"""
+    pickle.dump(target_obj, open(filename, 'wb'))
